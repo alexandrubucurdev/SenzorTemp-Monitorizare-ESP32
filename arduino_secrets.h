af4d699f13efd15c86e9371e113292b249586cde
Bucur Alexandru <alexandru.bucur@bestcj.ro>
2025-11-21 22:11:08 +0200

    Sterge fisierul secrets din repo

--- arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-#define SECRET_SSID "TP-Link_3C60"                    // numele retelei tale Wi-Fi
-#define SECRET_OPTIONAL_PASS "99212841"               // parola Wi-Fi
-#define SECRET_DEVICE_KEY "ij8A5Kvw9VZMHRqfXr8ytWbVO" // cheia generata de Arduino Cloud pentru device
